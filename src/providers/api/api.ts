70a7d5d009a46beefb19ff026db142899022cd37
alejandroantivero <alejandro.antivero@gmail.com>
2017-12-18 11:24:30 -0300

    llamada back end

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -6,7 +6,8 @@ import { Injectable } from '@angular/core';
  */
 @Injectable()
 export class Api {
-  public static API_URL: string = 'http://localhost:9090/api';
+  //public static API_URL: string = 'http://localhost:9090/api';
+  public static API_URL: string = 'https://debinapp-1.herokuapp.com/api';
 
   constructor(public http: HttpClient) {
   }
